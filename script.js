e76e0571f62c77ecc9903ae77dd6b4eea0712a94
Mario-Mellek <mario.samy266@gmail.com>
2023-02-06 13:11:55 +0200

    Flip-button added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const time = document.getElementsByClassName('time')[0];
 const alarmTime = document.getElementById('alarm-time--minutes');
 const alarmTimeSeconds = document.getElementById('alarm-time--seconds');
 const ringTone = new Audio('./assets/Alarm.mp3');
+const flipbtn = document.getElementsByTagName('small')[0];
 
 let secondsInterval;
 let minutes = 0;
@@ -42,6 +43,22 @@ resetBtn.addEventListener('click', () => {
   minutes = seconds = 0;
   startBtn.innerHTML = 'Start';
   alarmTime.value = alarmTimeSeconds.value = '0';
+});
+
+flipbtn.addEventListener('click', () => {
+  const section = document.getElementsByTagName('section')[0];
+  if (section.classList[0] === undefined) {
+    section.classList.add('fFlip');
+    return;
+  }
+  if (section.classList.contains('bFlip')) {
+    section.classList.replace('bFlip', 'fFlip');
+    return;
+  }
+  if (section.classList.contains('fFlip')) {
+    section.classList.replace('fFlip', 'bFlip');
+    return;
+  }
 });
 
 function alarm() {
