3cfbda644c3817d427397461634269af1ac163d8
Mario-Mellek <mario.samy266@gmail.com>
2023-02-07 14:24:48 +0200

    Created a second script for coundown

---
+++ script1.js
@@ -0,0 +1,8 @@
+const timerSection = document.getElementsByTagName('section')[1];
+const timerInput = document.getElementById('watch-time--minutes').value;
+const timerInputSeconds = document.getElementById('watch-time--seconds').value;
+const countDown_startBtn = document.getElementsByClassName('start')[1];
+const countDown_resetBtn = document.getElementsByClassName('reset')[1];
+const countDown_time = document.getElementsByClassName('time')[1];
+
+console.log(countDown_time);
